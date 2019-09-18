51f31e41ab236851dd95ecf3d187196d58f16258
William Phokompe <phokompe.william@outlook.com>
2019-09-18 11:02:21 +0200

    Add route for polls app and add  an index template

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from .models import Question, Choice
 
 def index(request):
-    return render(request, 'polls/index.html')
+    return render(request, 'polls/index.htm')

8bd72c10a65a535325d75e101f7afc3c5d85ce46
William Phokompe <phokompe.william@outlook.com>
2019-09-18 14:00:52 +0200

    Create the vote template method
    Included a redirect response after voting to disable double sumbmission

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -21,3 +21,22 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.htm', { 'question': question })
+
+# Vote for a question of choice
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form
+        return render(request, 'polls/details.htm', {
+            'question': question,
+            'error_message': 'You didn\'t select a choice'
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the back button
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))

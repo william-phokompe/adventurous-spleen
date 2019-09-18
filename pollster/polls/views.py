8a6419dca76ca6755b8cada9c447e899a45a887c
William Phokompe <phokompe.william@outlook.com>
2019-09-18 11:49:18 +0200

    <!-- Read comments -->

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Question, Choice
 
+# Get questions and display
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = { 'latest_question_list': latest_question_list }
     return render(request, 'polls/index.htm', context)
+
+# Show questions and choices to vote for
